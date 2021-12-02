7dbc98ace23941a7dbc9bb1d1573629f1098a03d
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-12-02 11:13:21 -0600

    coded route to find all workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,32 +2,48 @@ const router = require("express").Router();
 const db = require("../models");
 
 // code route to create a new workout
-router.post('/api/workouts', ({ body }. res) => {
-    db.Workout.create(body).then(dbWorkout => {
-        res.json(dbWorkout);
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
-        res.status(400).json(err);
+    .catch((err) => {
+      res.status(400).json(err);
     });
 });
 
 // code route to update an existing workout
-router.put('/api/workouts/:id', (req, res) => {
-    console.log(req.body)
-    console.log(req.params)
-    db.Workout.findByIdAndUpdate(
-        req.params.id,
-        { $push: {exercises: req.body} },
-        { upsert: true }
-    )
-    .then(dbWorkout => {
-        res.json(dbWorkout);
+router.put("/api/workouts/:id", (req, res) => {
+  console.log(req.body);
+  console.log(req.params);
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    { upsert: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
-        res.status(400).json(err);
+    .catch((err) => {
+      res.status(400).json(err);
     });
 });
 
 // code route to find all workouts
+router.get("/api/workouts", (req, res) => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDurationg: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 // code route to find the details of the past 7 workouts
