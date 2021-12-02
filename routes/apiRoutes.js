5bd7c7c8579eeacacf91295a1d30b9a9ced0d17f
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-12-02 11:00:17 -0600

    coded api route to create new workout. haven't been able to connect to heroku, can't tell if it works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 const db = require("../models");
 
 // code route to create a new workout
+router.post('/api/workouts', ({ body }. res) => {
+    db.Workout.create(body).then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 // code route to update an existing workout
 
