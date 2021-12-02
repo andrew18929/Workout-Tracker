289ffdac393bd9ff50d0bfaa4b3f712863c57c8d
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-12-02 10:52:12 -0600

    started apiRoutes.js and pseudocoded routes needed

---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const db = require("../models");
+
+// code route to create a new workout
+
+// code route to update an existing workout
+
+// code route to find all workouts
+
+// code route to find the details of the past 7 workouts
